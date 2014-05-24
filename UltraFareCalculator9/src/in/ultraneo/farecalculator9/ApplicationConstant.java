32cec4ff6e783c190974bfc2391cdaa41b028cce
Narayanan Chandran <cnarayan12ster@gmail.com>
2014-05-24 13:00:45 +0530

    SearchActivity Added Like Google Now Only Above [Search Box Added][UI][No Code is written for it]

--- UltraFareCalculator9/src/in/ultraneo/farecalculator9/ApplicationConstant.java
+++ UltraFareCalculator9/src/in/ultraneo/farecalculator9/ApplicationConstant.java
@@ -4,3 +4,4 @@ public class ApplicationConstant {
 	static final int SourceRequest = 1001;
 	static final int DestinationRequest = 1002;
 }
+ 
---
+++ UltraFareCalculator9/src/in/ultraneo/farecalculator9/SearchPlaceActivity.java
@@ -0,0 +1,15 @@
+package in.ultraneo.farecalculator9;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SearchPlaceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_place);     
+	}
+}
